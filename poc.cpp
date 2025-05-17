8970696af57b5d4aa3d607d49521feeebebe92a9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-17 12:10:47 +0100

    poc: adding more examples

--- poc.cpp
+++ poc.cpp
@@ -3,9 +3,23 @@
 import hay;
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// Windows-specific usage example
+class PWSTR {};
+void CoDelete(PWSTR) {}
+PWSTR CoAlloc() { return {}; }
+void CoSomething(PWSTR) {}
+
+template<typename T> T ident(T t) { return t; }
 
 int main() {
   using file = hay<FILE *, fopen, fclose>;
-
   fputs("ok", file { "out/test.txt", "w" });
+
+  using pwstr = hay<PWSTR, ident<PWSTR>, CoDelete>;
+  CoSomething(pwstr { CoAlloc() });
+
+  using malloced = hay<void *, malloc, free>;
+  printf("%p\n", static_cast<void *>(malloced { 100 }));
 }
